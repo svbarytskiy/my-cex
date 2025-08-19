fd75e3652897680442bc920edd30973c0d669fc4
Sviatoslav Barytskyi <yujknjdjjdhh67@gmail.com>
2025-08-19 19:09:07 +0300

    ci: add GitHub Actions workflow

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-
+  base: '/my-cex/',
   resolve: {
     alias: {
       common: '/src/common',
