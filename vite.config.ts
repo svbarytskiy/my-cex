b2025be55af049262e8f10667acea3bcd03f9139
Sviatoslav Barytskiy <115037923+svbarytskiy@users.noreply.github.com>
2025-10-01 15:45:28 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: process.env.NODE_ENV === 'production' ? '/my-cex/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/' : '/',
   resolve: {
     alias: {
       common: '/src/common',
